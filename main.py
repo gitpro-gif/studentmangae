d63c4165e91df72bcceba9d01ed2f9f9956a2450
gitpro-gif <186488259+gitpro-gif@users.noreply.github.com>
2026-05-23 12:33:06 +0530

    practicing PR

--- main.py
+++ main.py
@@ -5,72 +5,22 @@ app = FastAPI()
 students = []
 
 @app.get("/")
-def home():
-    return {"message" : "Student Management System Running"}
+def CheckHealth():
+    return {"message": "App is working"}
 
-@app.get("/students")
-def get_students():
-    return students
 
-@app.post("/create_student")
-def add_student(id: int, name: str, age: int, course: str):
+@app.post("/add")
+def AddStudent(id: int, name: str, rollno: int):
     
     student = {
         "id": id,
         "name": name,
-        "age": age,
-        "course": course
+        "rollno": rollno
     }
     
     students.append(student)
-    
     return {
-        "message" : "Student added successfull",
-        "status code" : 200,
+        "message" : "Student added",
         "data": student
     }
     
-
-@app.get("/student/{id}")
-def get_by_id(id: int):
-
-    for student in students:
-
-        if student["id"] == id:
-
-            return {
-                "message": "Student found",
-                "data": student
-            }
-
-    return {
-        "message": "Student not found"
-    }
-    
-
-@app.put("/update_student/{id}")
-def update_student(id: int, new_name: str):
-    
-    for student in students:
-        if student["id"] == id:
-            
-            student["name"] = new_name
-            return {
-                "message" : "Data updated successfully",
-                "data": student
-            }
-    return {"message" : "Student Not found"}
-
-
-@app.delete("/delete/{id}")
-def delete_student(id: int):
-    
-    for student in students:
-        if student["id"] == id:
-            
-            students.remove(student)
-            return {
-                "message" : "deleted successfully"
-            }
-            
-    return {"message": "Student not found"}
